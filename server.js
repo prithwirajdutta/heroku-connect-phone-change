84c856f9e74d81974e136f384420a40ae62ef1f6
Prithwiraj Dutta <duttaprithwiraj9@gmail.com>
2020-09-30 18:32:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,10 +9,9 @@ app.set('port', process.env.PORT || 5000);
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
-var pg_db_uri = "https://data.heroku.com/datastores/d5322ad6-1d3e-4090-8929-370a3021bd26";
 
 app.post('/update', function(req, res) {
-    pg.connect(pg_db_uri, function (err, conn, done) {
+    pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
         // watch for any connect issues
         if (err) console.log(err);
         conn.query(
